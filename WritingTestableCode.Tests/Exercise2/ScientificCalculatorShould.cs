bdc250a3ee9dbf97267e1af0613a26879ff73fc2
Alex Tercete <alextercete@gmail.com>
2019-03-02 23:54:50 +0000

    Isolate access to SUT

--- WritingTestableCode.Tests/Exercise2/ScientificCalculatorShould.cs
+++ WritingTestableCode.Tests/Exercise2/ScientificCalculatorShould.cs
@@ -7,121 +7,125 @@ namespace WritingTestableCode.Tests.Exercise2
     [TestFixture]
     public class ScientificCalculatorShould
     {
+        private ScientificCalculator _calculator;
+
+        [SetUp]
+        public void Set_up()
+        {
+            _calculator = new ScientificCalculator();
+        }
+
         [Test]
         public void Add_two_numbers()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(2));
+            Input(new Operator((a, b) => a + b));
+            Input(new Number(3));
+            Input(new Operator((a, b) => b));
 
-            calculator.Input(new Number(2));
-            calculator.Input(new Operator((a, b) => a + b));
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => b));
-
-            calculator.Display.Should().Be(5);
+            Result.Should().Be(5);
         }
 
         [Test]
         public void Subtract_two_numbers()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(5));
+            Input(new Operator((a, b) => a - b));
+            Input(new Number(3));
+            Input(new Operator((a, b) => b));
 
-            calculator.Input(new Number(5));
-            calculator.Input(new Operator((a, b) => a - b));
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => b));
-
-            calculator.Display.Should().Be(2);
+            Result.Should().Be(2);
         }
 
         [Test]
         public void Multiply_two_numbers()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(2));
+            Input(new Operator((a, b) => a * b));
+            Input(new Number(3));
+            Input(new Operator((a, b) => b));
 
-            calculator.Input(new Number(2));
-            calculator.Input(new Operator((a, b) => a * b));
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => b));
-
-            calculator.Display.Should().Be(6);
+            Result.Should().Be(6);
         }
 
         [Test]
         public void Divide_two_numbers()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(6));
+            Input(new Operator((a, b) => a / b));
+            Input(new Number(3));
+            Input(new Operator((a, b) => b));
 
-            calculator.Input(new Number(6));
-            calculator.Input(new Operator((a, b) => a / b));
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => b));
-
-            calculator.Display.Should().Be(2);
+            Result.Should().Be(2);
         }
 
         [Test]
         public void Use_the_result_from_a_previous_operation_in_a_subsequent_operation()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(2));
+            Input(new Operator((a, b) => a + b));
+            Input(new Number(3));
+            Input(new Operator((a, b) => a - b));
+            Input(new Number(1));
+            Input(new Operator((a, b) => a * b));
+            Input(new Number(4));
+            Input(new Operator((a, b) => a / b));
+            Input(new Number(8));
+            Input(new Operator((a, b) => b));
 
-            calculator.Input(new Number(2));
-            calculator.Input(new Operator((a, b) => a + b));
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => a - b));
-            calculator.Input(new Number(1));
-            calculator.Input(new Operator((a, b) => a * b));
-            calculator.Input(new Number(4));
-            calculator.Input(new Operator((a, b) => a / b));
-            calculator.Input(new Number(8));
-            calculator.Input(new Operator((a, b) => b));
-
-            calculator.Display.Should().Be(2);
+            Result.Should().Be(2);
         }
 
         [Test]
         public void Display_partial_results_for_multi_step_operation()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(3));
+            Input(new Operator((a, b) => a + b));
+            Input(new Number(4));
+            Input(new Operator((a, b) => a - b));
 
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => a + b));
-            calculator.Input(new Number(4));
-            calculator.Input(new Operator((a, b) => a - b));
-
-            calculator.Display.Should().Be(7);
+            Result.Should().Be(7);
         }
 
         [Test]
         public void Refresh_result()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(1));
+            Input(new Operator((a, b) => b));
+            Input(new Number(2));
+            Input(new Operator((a, b) => b));
 
-            calculator.Input(new Number(1));
-            calculator.Input(new Operator((a, b) => b));
-            calculator.Input(new Number(2));
-            calculator.Input(new Operator((a, b) => b));
-
-            calculator.Display.Should().Be(2);
+            Result.Should().Be(2);
         }
 
         [Test]
         public void Have_a_memory()
         {
-            var calculator = new ScientificCalculator();
+            Input(new Number(10));
+            Input(new Operator((a, b) => a + b));
+            Input(new Number(5));
+            Input(new MemoryFunction("Add"));
+            Clear();
+            Input(new Number(12));
+            Input(new MemoryFunction("Subtract"));
+            Clear();
+            Input(new Number(3));
+            Input(new Operator((a, b) => a + b));
+            Input(new MemoryFunction("Recall"));
 
-            calculator.Input(new Number(10));
-            calculator.Input(new Operator((a, b) => a + b));
-            calculator.Input(new Number(5));
-            calculator.Input(new MemoryFunction("Add"));
-            calculator.Clear();
-            calculator.Input(new Number(12));
-            calculator.Input(new MemoryFunction("Subtract"));
-            calculator.Clear();
-            calculator.Input(new Number(3));
-            calculator.Input(new Operator((a, b) => a + b));
-            calculator.Input(new MemoryFunction("Recall"));
-
-            calculator.Display.Should().Be(6);
+            Result.Should().Be(6);
         }
+
+        private void Input(ICalculatorInput input)
+        {
+            _calculator.Input(input);
+        }
+
+        private void Clear()
+        {
+            _calculator.Clear();
+        }
+
+        private double Result => _calculator.Display;
     }
 }
