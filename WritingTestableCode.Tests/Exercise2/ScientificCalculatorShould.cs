473c7a62beaba0d5383b644cbebe455166331e55
Alex Tercete <alextercete@gmail.com>
2019-03-03 00:42:55 +0000

    Inline input factory methods for simplicity

--- WritingTestableCode.Tests/Exercise2/ScientificCalculatorShould.cs
+++ WritingTestableCode.Tests/Exercise2/ScientificCalculatorShould.cs
@@ -18,28 +18,28 @@ namespace WritingTestableCode.Tests.Exercise2
         [Test]
         public void Add_two_numbers()
         {
-            Input(Number(2), AddOperator, Number(3), EqualOperator);
+            Input(Number(2), Operator.Add, Number(3), Operator.Equal);
             Result.Should().Be(5);
         }
 
         [Test]
         public void Subtract_two_numbers()
         {
-            Input(Number(5), SubtractOperator, Number(3), EqualOperator);
+            Input(Number(5), Operator.Subtract, Number(3), Operator.Equal);
             Result.Should().Be(2);
         }
 
         [Test]
         public void Multiply_two_numbers()
         {
-            Input(Number(2), MultiplyOperator, Number(3), EqualOperator);
+            Input(Number(2), Operator.Multiply, Number(3), Operator.Equal);
             Result.Should().Be(6);
         }
 
         [Test]
         public void Divide_two_numbers()
         {
-            Input(Number(6), DivideOperator, Number(3), EqualOperator);
+            Input(Number(6), Operator.Divide, Number(3), Operator.Equal);
             Result.Should().Be(2);
         }
 
@@ -47,11 +47,11 @@ namespace WritingTestableCode.Tests.Exercise2
         public void Use_the_result_from_a_previous_operation_in_a_subsequent_operation()
         {
             Input(Number(2));
-            Input(AddOperator, Number(3));
-            Input(SubtractOperator, Number(1));
-            Input(MultiplyOperator, Number(4));
-            Input(DivideOperator, Number(8));
-            Input(EqualOperator);
+            Input(Operator.Add, Number(3));
+            Input(Operator.Subtract, Number(1));
+            Input(Operator.Multiply, Number(4));
+            Input(Operator.Divide, Number(8));
+            Input(Operator.Equal);
 
             Result.Should().Be(2);
         }
@@ -59,47 +59,31 @@ namespace WritingTestableCode.Tests.Exercise2
         [Test]
         public void Display_partial_results_for_multi_step_operation()
         {
-            Input(Number(3), AddOperator, Number(4), EqualOperator);
+            Input(Number(3), Operator.Add, Number(4), Operator.Equal);
             Result.Should().Be(7);
         }
 
         [Test]
         public void Refresh_result()
         {
-            Input(Number(1), EqualOperator, Number(2), EqualOperator);
+            Input(Number(1), Operator.Equal, Number(2), Operator.Equal);
             Result.Should().Be(2);
         }
 
         [Test]
         public void Have_a_memory()
         {
-            Input(Number(10), AddOperator, Number(5), AddMemoryFunction);
+            Input(Number(10), Operator.Add, Number(5), MemoryFunction.Add);
             Clear();
-            Input(Number(12), SubtractMemoryFunction);
+            Input(Number(12), MemoryFunction.Subtract);
             Clear();
             Input(Number(3));
-            Input(AddOperator, RecallMemoryFunction);
+            Input(Operator.Add, MemoryFunction.Recall);
 
             Result.Should().Be(6);
         }
 
         private static ICalculatorInput Number(double value) => new Number(value);
-
-        private static ICalculatorInput AddOperator => Operator.Add;
-
-        private static ICalculatorInput SubtractOperator => Operator.Subtract;
-
-        private static ICalculatorInput MultiplyOperator => Operator.Multiply;
-
-        private static ICalculatorInput DivideOperator => Operator.Divide;
-
-        private static ICalculatorInput EqualOperator => Operator.Equal;
-
-        private static ICalculatorInput AddMemoryFunction => MemoryFunction.Add;
-
-        private static ICalculatorInput SubtractMemoryFunction => MemoryFunction.Subtract;
-
-        private static ICalculatorInput RecallMemoryFunction => MemoryFunction.Recall;
 
         private void Input(params ICalculatorInput[] inputs)
         {
